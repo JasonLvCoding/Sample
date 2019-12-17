4e1bf0dc94b8f8e8bfb11cf7a8103caac76d7fe5
zhangsong <zhangsong@beeboxes.net>
2019-12-17 11:15:30 +0800

    update layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <router-view name="header"/>
+    <div class="page-wrap">
+      <div class="page-content">
+        <router-view name="sidebar"/>
+        <router-view/>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app .page-wrap {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 50px 0 0 0;
+  margin: 0;
+}
+#app .page-content{
+  display: flex;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
